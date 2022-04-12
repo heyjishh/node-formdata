7fbecf14952babf4dc501fc8d666a5b46d658acf
Jishan Anwar <anwarjishan321@gmail.com>
2022-04-12 13:20:11 +0530

    updated db 1

--- server.js
+++ server.js
@@ -111,23 +111,6 @@ app.get("/", function (req, res) {
 	});
 });
 
-//
-// route code
-// var mascots = [
-// 	{ name: "Sammy", organization: "DigitalOcean", birth_year: 2012 },
-// 	{ name: "Tux", organization: "Linux", birth_year: 1996 },
-// 	{ name: "Moby Dock", organization: "Docker", birth_year: 2013 },
-// ];
-// var tagline =
-// 	"No programming concept is complete without a cute animal mascot.";
-
-// res.render("pages/index", {
-// mascots: mascots,
-// tagline: tagline,
-// });
-
-///
-
 app.listen(PORT, () => {
 	console.log(`service is running on http://localhost:${PORT}`);
 });
